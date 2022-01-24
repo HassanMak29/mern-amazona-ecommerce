f831e2e1e7bf6712ccc6e33bd5c5930070d2a24e
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2022-01-24 22:23:08 +0100

    added multiple pages and features

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,50 @@
+/* eslint-disable no-undef */
 import express from "express";
-import data from "./data.js";
+import mongoose from "mongoose";
+// import data from "./data.js";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const DB =
+  "mongodb+srv://hassan:test1234@cluster0.web3m.mongodb.net/amazona?retryWrites=true&w=majority";
+
+// const DB1 = "mongodb://localhost/amazona";
+
+// eslint-disable-next-line no-undef
+mongoose.connect(process.env.MONGODB_URL || DB);
+
+// app.get("/api/products/:id", (req, res) => {
+//   const product = data.products.find((prod) => prod._id === req.params.id);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: "Product not found" });
+//   }
+// });
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
+});
+
+app.use((err, req, res) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5500;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,106 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
+import CartScreen from "./Screens/CartScreen";
+import SigninScreen from "./Screens/SigninScreen";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signout } from "./actions/userActions";
+import RegisterScreen from "./Screens/RegisterScreen";
+import ShippingAddressScreen from "./Screens/ShippingAddressScreen";
+import PaymentMethodScreen from "./Screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./Screens/PlaceOrderScreen";
+import OrderScreen from "./Screens/OrderScreen";
+import OrderHistoryScreen from "./Screens/OrderHistoryScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  console.log("hello");
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
 
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               amazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderslist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/order/:id" component={OrderScreen}></Route>
+          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+          ></PrivateRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
